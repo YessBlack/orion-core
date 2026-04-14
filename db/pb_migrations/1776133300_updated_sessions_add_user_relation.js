cc9c449ac412eee2ad3a37fd057b371a2e0da32e
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-04-14 12:57:24 -0500

    Update db migrations

--- db/pb_migrations/1776133300_updated_sessions_add_user_relation.js
+++ db/pb_migrations/1776133300_updated_sessions_add_user_relation.js
@@ -2,6 +2,9 @@
 migrate((app) => {
   const collection = app.findCollectionByNameOrId("pbc_3660498186")
 
+  // replace previous optional userId field from 1776133227 migration
+  collection.fields.removeById("relation1689669068")
+
   // add field
   collection.fields.addAt(1, new Field({
     "cascadeDelete": false,
@@ -23,6 +26,21 @@ migrate((app) => {
 
   // remove field
   collection.fields.removeById("relation1172758201")
+
+  // restore previous optional userId field
+  collection.fields.addAt(4, new Field({
+    "cascadeDelete": false,
+    "collectionId": "_pb_users_auth_",
+    "hidden": false,
+    "id": "relation1689669068",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "userId",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "relation"
+  }))
 
   return app.save(collection)
 })
